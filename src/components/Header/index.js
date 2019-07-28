f313069784bc6e6d9048192afdca8842165713f8
lml <soalin228@163.com>
2019-07-29 07:52:06 +0800

    header 完成

---
+++ src/components/Header/index.js
@@ -0,0 +1,32 @@
+import React, {Component, Fragment} from 'react'
+import {Menu, Icon} from 'antd'
+import logo from './logo.png'
+import './style.css'
+
+class AppHeader extends Component {
+  render () {
+    return (
+      <Fragment>
+        <img src={logo} alt="" className={'app-header-logo'}/>
+        <Menu mode="horizontal"
+              className={'app-header-menu'}
+        >
+          <Menu.Item key="mail">
+            <Icon type="mail"/>one
+          </Menu.Item>
+          <Menu.Item key="mail2">
+            <Icon type="mail"/>one
+          </Menu.Item>
+          <Menu.Item key="mail3">
+            <Icon type="mail"/>one
+          </Menu.Item>
+          <Menu.Item key="mail4">
+            <Icon type="mail"/>one
+          </Menu.Item>
+        </Menu>
+      </Fragment>
+    )
+  }
+}
+
+export default AppHeader
--- src/index.js
+++ src/index.js
@@ -1,6 +1,26 @@
-import React from 'react'
+import React, {Component} from 'react'
 import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
+import {Layout} from 'antd'
+import AppHeader from './components/Header'
+
+import 'antd/dist/antd.css'
+import './style.css'
+
+const { Header, Footer, Content } = Layout
+
+
+class App extends Component {
+  render () {
+    return (
+      <Layout style={{ minWidth: 1300 }}>
+        <Header className={'header'}>
+          <AppHeader/>
+        </Header>
+        <Content className={'content'}>Content</Content>
+        <Footer className={'footer'}>Footer</Footer>
+      </Layout>
+    )
+  }
+}
 
 ReactDOM.render(<App/>, document.getElementById('root'))
